733691fe35616f96bbcde5ee53e76dd26c0827e6
PEDRO ELIAS MELLO POSSAMAI <pedro_e_possamai@estudante.sesisenai.org.br>
2025-05-06 21:44:00 -0300

    att

--- Sistema/Backend/validaLogin.php
+++ Sistema/Backend/validaLogin.php
@@ -15,34 +15,27 @@
     //$_SESSION - Variável criada pelo usuário no PHP
 
     include("conexao.php");
-    $sql = "SELECT * FROM usuarios "
-            ." WHERE login = '$email' "
-            ." AND senha = md5('$senha');";
+    $sql = "SELECT * FROM usuario
+            WHERE email = '$email'
+            AND senha = md5('$senha');";
     $resultLogin = mysqli_query($conn,$sql);
     mysqli_close($conn);
 
     //Validar se tem retorno do BD
     if (mysqli_num_rows($resultLogin) > 0) {  
-        
-        //enviarEmail('destino@email.com.br','Mensagem de e-mail para SA','Teste SA','Eu mesmo');
-
         foreach ($resultLogin as $coluna) {
                         
             //***Verificar os dados da consulta SQL
-            $_SESSION['idTipoUsuario'] = $coluna['idTipoUsuario'];
-            $_SESSION['logado']        = 1;
-            $_SESSION['idLogin']       = $coluna['idUsuario'];
-            $_SESSION['NomeLogin']     = $coluna['Nome'];
-            $_SESSION['FotoLogin']     = $coluna['Foto'];
-            $_SESSION['AtivoLogin']    = $coluna['FlgAtivo'];
+            $_SESSION['idUsuario']       = $coluna['id_usuario'];
+            //$_SESSION['logado']        = 1;
 
             //Acessar a tela inicial
-            //header('location: ../painel.php');
+            header('location: ../Frontend/sidebar/index.html');
             
         }        
     }else{
         //Acessar a tela inicial
-        //header('location: ../');
+        header('location: ../../index.php');
     } 
 
     
