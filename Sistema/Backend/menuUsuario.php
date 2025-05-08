6bb246e40459ee2809a1ddfeecee32faa5bd3ee9
HENRIQUE WHITE <henrique_white@estudante.sesisenai.org.br>
2025-05-07 22:12:26 -0300

    att

--- Sistema/Backend/menuUsuario.php
+++ Sistema/Backend/menuUsuario.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Perfil de Usuário</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/style-menuUsuario.css">
     </head>
 
     <section class="content">
@@ -34,11 +34,11 @@
                                             <div class="row">
                                                 <div class="col-3 text-center">
                                                     <div class="foto-perfil mx-auto">
-                                                        <img alt=" NomeLogin " src="imagens/logo.png" class="foto">
+                                                        <img alt="<?php echo $_SESSION['NomeLogin'];?>" src="img/logo.png" class="foto">
                                                         <div class="trocar-imagem">
                                                             <i class="fas fa-camera upload-button"></i>
                                                             <p>Alterar Foto</p>
-                                                            <input class="arquivo" name="Foto" type="file" title="" accept="imagens/*"/>
+                                                            <input class="arquivo" name="Foto" type="file" title="" accept="image/*"/>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -46,32 +46,32 @@
                                                     <div class="row">
                                                         <div class="col-7">
                                                             <div class="form-group">
-                                                                <label for="iNome">Nome:</label>
+                                                                <label for="iNome">Nome</label>
                                                                 <input name="nNome" id="iNome" type="text" class="form-control" value=" Nome" required>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-5">
-                                                            <div class="form-group">
-                                                                <label>Empresa:</label>
-                                                                <input readonly name="nEmpresa" id="iEmpresa" type="text" class="form-control" value=" Empresa " required>
                                                             </div>
                                                         </div>
                                                         <div class="col-7">
                                                             <div class="form-group">
-                                                                <label>Email:</label>
+                                                                <label>Senha</label>
+                                                                <input name="nSenha" id="iSenha" type="text" class="form-control" value=" Senha " required>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-7">
+                                                            <div class="form-group">
+                                                                <label>Email</label>
                                                                 <input readonly name="nEmail" id="iEmail" type="email" class="form-control" value=" Email " required>
                                                             </div>
                                                         </div>
                                                         <div class="col-5">
                                                             <div class="form-group">
-                                                                <label>Tipo de Usuário:</label>
-                                                                <input readonly name="nTipo" id="iTipo" type="text" class="form-control" value=" Tipo do Usuário " required>
+                                                                <label>Empresa</label>
+                                                                <input readonly name="nEmpresa" id="iEmpresa" type="text" class="form-control" value=" Empresa " required>
                                                             </div>
                                                         </div>
-                                                        <div class="col-7">
+                                                        <div class="col-5">
                                                             <div class="form-group">
-                                                                <label>Senha:</label>
-                                                                <input name="nSenha" id="iSenha" type="text" class="form-control" value=" Senha " required>
+                                                                <label>Tipo de Usuário</label>
+                                                                <input readonly name="nTipo" id="iTipo" type="text" class="form-control" value=" Tipo do Usuário " required>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -83,10 +83,10 @@
                             </div>
                         </div>
                         <div class="card-action" align="right">
-                            <a href="index.php" class="btn btn-danger" data-toggle="tooltip" title="Limpar as Alterações">
+                            <a href="menuUsuario.php" class="btn btn-danger" data-toggle="tooltip" title="Limpar Alterações">
                                 <span>Limpar</span>
                             </a>
-                            <input type="submit" class="btn btn-success" value="Salvar" data-toggle="tooltip" title="Salvar as Alterações">
+                            <input type="submit" class="btn btn-success" value="Salvar" data-toggle="tooltip" title="Salvar Alterações">
                         </div>
                         </div>
                 </form>
