3705f5c717138a61422c0fdc85d8b0c98a7b2530
leopessoa10 <leopessoacwb@gmail.com>
2025-05-19 21:56:12 -0300

    CRUD vagas pronto

--- Sistema/vagas.php
+++ Sistema/vagas.php
@@ -48,15 +48,15 @@
                 <div class="row">
                   
                   <div class="col-6">
-                    <h3 class="card-title">Produtos</h3>
+                    <h3 class="card-title">Vagas</h3>
                   </div>
                   
                   <div class="col-6" align="right">
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#testeAjaxModal">
                       Teste Ajax
                     </button>
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#novoProdutoModal">
-                      Novo Produto
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#novaVagaModal">
+                      Nova Vaga
                     </button>
                   </div>
 
@@ -71,15 +71,16 @@
                   <thead>
                   <tr>
                       <th>ID</th>
-                      <th>Produto</th>
-                      <th>Categoria</th>
-                      <th>Quantidade</th>               
+                      <th>Descrição</th>
+                      <th>Situação</th>
+                      <th>Empresa</th>               
+                      <th>Ativo</th>               
                       <th>Ações</th>
                   </tr>
                   </thead>
                   <tbody>
 
-                  <?php echo listaProduto(); ?>
+                  <?php echo listaVagas(); ?>
                   
                   </tbody>
                   
@@ -96,17 +97,17 @@
       </div>
       <!-- /.container-fluid -->
 
-      <div class="modal fade" id="novoProdutoModal">
+      <div class="modal fade" id="novaVagaModal">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header bg-success">
-              <h4 class="modal-title">Novo Produto</h4>
+              <h4 class="modal-title">Nova Vaga</h4>
               <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              <form method="POST" action="backend/salvarProduto.php?funcao=I" enctype="multipart/form-data">              
+              <form method="POST" action="backend/salvarVaga.php?funcao=I" enctype="multipart/form-data">              
                 
                 <div class="row">
                   <div class="col-12">
@@ -116,20 +117,27 @@
                     </div>
                   </div>
 
+                  <div class="col-12">
+                    <div class="form-group">
+                      <label for="iSituacao">Situação:</label>
+                      <input type="text" class="form-control" id="iSituacao" name="nSituacao" maxlength="80">
+                    </div>
+                  </div>
+
                   <div class="col-8">
                     <div class="form-group">
-                      <label for="iCategoria">Categoria:</label>
-                      <select name="nCategoria" id="iCategoria" class="form-control" required>
+                      <label for="iEmpresa">Empresa:</label>
+                      <select name="nEmpresa" id="iEmpresa" class="form-control" required>
                         <option value="">Selecione...</option>
-                        <?php echo optionCategoria();?>
+                        <?php echo optionEmpresa();?>
                       </select>
                     </div>
                   </div>
 
-                  <div class="col-4">
+                  <div class="col-12">
                     <div class="form-group">
-                      <label for="iQuantidade">Quantidade:</label>
-                      <input type="number" class="form-control" id="iQuantidade" name="nQuantidade" min="0">
+                      <input type="checkbox" id="iAtivo" name="nAtivo">
+                      <label for="iAtivo">Usuário Ativo</label>
                     </div>
                   </div>
 
@@ -149,62 +157,7 @@
         </div>
         <!-- /.modal-dialog -->
       </div>
-      <!-- /.modal -->
-
-      <div class="modal fade" id="testeAjaxModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header bg-primary">
-              <h4 class="modal-title">Teste Ajax</h4>
-              <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form method="POST" action="#" enctype="multipart/form-data">              
-                
-                <div class="row">
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label for="iCategoriaAjax">Categoria:</label>
-                      <select name="nCategoriaAjax" id="iCategoriaAjax" class="form-control" required>
-                        <option value="">Selecione...</option>
-                        <?php echo optionCategoria();?>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div class="col-8">
-                    <div class="form-group">
-                      <label for="iProdutoAjax">Produtos:</label>
-                      <select name="nProdutoAjax" id="iProdutoAjax" class="form-control" required>
-                        <option value="">Selecione...</option>
-                      </select>
-                    </div>
-                  </div>
-
-                </div>
-
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
-                  <button type="submit" class="btn btn-success">Salvar</button>
-                </div>
-                
-              </form>
-
-            </div>
-            
-          </div>
-          <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
-      </div>
-      <!-- /.modal -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-
+      
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
@@ -217,68 +170,6 @@
 <?php include('partes/js.php'); ?>
 <!-- Fim JS -->
 
-<script>
-  //== Inicialização
-  $(document).ready(function() {
-
-    //Lista dinâmica com Ajax
-    $('#iCategoriaAjax').on('change',function(){
-			//Pega o valor selecionado na lista 1
-      var categoria  = $('#iCategoriaAjax').val();
-      
-      //Prepara a lista 2 filtrada
-      var optionProd = '';
-                
-      //Valida se teve seleção na lista 1
-      if(categoria != "" && categoria != "0"){
-        
-        //Vai no PHP consultar dados para a lista 2
-        $.getJSON('backend/carregaProdutoCategoria.php?categoria='+categoria,
-        function (dados){  
-          
-          //Carrega a primeira option
-          optionProd = '<option value="">Selecione um Produto</option>';                  
-          
-          //Valida o retorno do PHP para montar a lista 2
-          if (dados.length > 0){                        
-            
-            //Se tem dados, monta a lista 2
-            $.each(dados, function(i, obj){
-              optionProd += '<option value="'+obj.idProduto+'">'+obj.Descricao+'</option>';	                            
-            })
-
-            //Marca a lista 2 como required e mostra os dados filtrados
-            $('#iProdutoAjax').attr("required", "req");						
-            $('#iProdutoAjax').html(optionProd).show();
-          }else{
-            
-            //Não encontrou itens para a lista 2
-            optionProd += '<option value="">Selecione um Produto</option>';
-            $('#iProdutoAjax').html(optionProd).show();
-          }
-        })                
-      }else{
-        //Sem seleção na lista 1 não consulta
-        optionProd += '<option value="">Selecione um Produto</option>';
-        $('#iProdutoAjax').html(optionProd).show();
-      }			
-		});
-  
-  });
-
-  $(function() {
-    $('#tabela').DataTable({
-      "paging": true,
-      "lengthChange": true,
-      "searching": true,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-
-</script>
 
 </body>
 </html>
